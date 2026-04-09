1e7875c3a26bcf99cf4e9f62a48f5fc694819140
Govher <sapardurdyyevagovher@gmail.com>
2026-04-08 22:24:46 -0500

    fixed audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import Home from './pages/home/home'
 import clickFile from './assets/click.mp3'
 
 export default function App() {
+  const audioRef = useRef(null);
+
   useEffect(() => {
-    const audio = new Audio(clickFile);
-    audio.volume = 0.5;
+    audioRef.current = new Audio(clickFile);
+    audioRef.current.volume = 0.5;
+    
+   
+    audioRef.current.load();
 
     const playClick = (e) => {
-      if(e.target.closest('button, a, .pixel-icon, .dock-icon')){
-        clickSound.currentTime = 0;
-        clickSound.play().catch(err => console.log("Audio play clocked by browser, click anywhere to start!"));
+      if (e.target.closest('button, a, .pixel-icon, .dock-icon')) {
+        const sound = audioRef.current;
+        if (sound) {
+          sound.currentTime = 0;
+          sound.play().catch(err => {
+            console.warn("Audio waiting for first user interaction...");
+          });
+        }
       }
     };
-    window.addEventListener('mousedown', playClick);
 
+    window.addEventListener('mousedown', playClick);
     return () => window.removeEventListener('mousedown', playClick);
-  },[]);
+  }, []);
+
   return <Home />
 }
